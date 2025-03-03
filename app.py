48132d9af716d16f3e0d495ad52ce8ec3d93512c
pickleManagerVDB <VDBpickleCup.management@gmail.com>
2025-03-03 00:33:55 -0500

    Update app.py for simple test

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -22,3 +23,19 @@ def cool():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10000)
+"""
+
+from flask import Flask
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Define a simple route to test if the server is working
+@app.route('/')  
+def test_server():
+    return "The server works!"  # This message will be displayed in the browser
+
+# Run the app (only when executed locally, not on Render)
+if __name__ == '__main__':
+    app.run(debug=True)
+
